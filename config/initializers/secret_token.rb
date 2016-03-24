6f8332ad9aa3a7826bf020f89c65f3fd788cc9da
SAKUMAISAO130 <colorfullweb@gmail.com>
2016-03-24 16:39:04 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Appppp::Application.config.secret_key_base = 'd05a63bc294035305bb28bb5f6dd015f6b6ebe6ff9289a0b339388119d0a5c2c0a5f84bd226ffba304db064255d1c4c6d4b15818d13b0728868610a1fc1ba7ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Appppp::Application.config.session_store :cookie_store, key: '_appppp_session'
