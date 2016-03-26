ab055f3507d8402c25f5fbc6210834056da9e22d
SAKUMAISAO130 <colorfullweb@gmail.com>
2016-03-26 12:37:55 +0900

    static fix-01

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+
+  # ページごとの完全なタイトルを返します。
+  def full_title(page_title)
+    #変数定義
+    base_title = "apppppp"
+    #ビューにあるpage_title変数が空の時
+    if page_title.empty?
+      base_title
+    else
+    #空じゃない時は棒線とタイトルをいい感じに出力する
+      "#{base_title} | #{page_title}"
+    end
+  end
+
+
 end
