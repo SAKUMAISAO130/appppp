80db092fe12af8e80d66d85cb335208b7f16aea5
SAKUMAISAO130 <colorfullweb@gmail.com>
2016-03-28 12:52:04 +0900

    userinfo-edit-create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+
+
   def show
     @user = User.find(params[:id])
   end
@@ -6,6 +12,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
 
   def create
     @user = User.new(user_params)
@@ -18,11 +31,45 @@ class UsersController < ApplicationController
     end
   end
 
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
+
+    # Before actions
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,6 +19,11 @@ module SessionsHelper
   end
 
 
+  def current_user?(user)
+    user == current_user
+  end
+
+
   def current_user
     remember_token = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
@@ -31,6 +36,14 @@ module SessionsHelper
   end
 
 
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url
+  end
 
 
 end
